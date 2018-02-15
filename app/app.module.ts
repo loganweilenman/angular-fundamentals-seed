8b910f62f2cbee72c3421e7eb80daeb142c2b7be
Logan Weilenman <loganweilenman@gmail.com>
2018-02-14 19:55:00 -0500

    complete crud app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 
 import { AppComponent } from './app.component';
 
+
+//could be created in own file
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'passengers',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
+
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
-    AppComponent
-  ]
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    //Angular Modules
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes),    
+    //RouterModule.forRoot(routes, { useHash: true }),
+    //Custom Modules
+    PassengerDashboardModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+//Containers
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
+//Components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+import { PassengerFormComponent } from './components/passenger-form/passenger-form.component';
+//Services
+import { PassengerDashboardService } from './passenger-dashboard.service';
+
+const routes: Routes = [
+    {
+        path: 'passengers',
+        children: [
+            { path: '', component: PassengerDashboardComponent },
+            { path: ':id', component: PassengerViewerComponent }
+        ]
+    }
+];
+
+@NgModule({
+    declarations: [
+        PassengerDashboardComponent,
+        PassengerViewerComponent,
+        PassengerCountComponent,
+        PassengerDetailComponent,
+        PassengerFormComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ],
+    providers: [
+        PassengerDashboardService
+    ]
+})
+
+export class PassengerDashboardModule {
+
+}
